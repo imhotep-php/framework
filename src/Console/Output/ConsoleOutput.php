79413e0c07f05b6e55c111734a3ef71269f2af58
Imhotep <imhotep.php@gmail.com>
2024-11-10 17:56:26 +0600

    Squashed commit of the following:
    commit 6338d3358c0bc3b6a73cf689a610d26f2fb3fef7
    Author: Imhotep <imhotep.php@gmail.com>
    Date:   Sun Nov 10 17:53:20 2024 +0600
    
        Fix get terminal dimensions
    
    commit 55131ebe6a21c4235ebd0800a2078a1c7a25aa76
    Author: Imhotep <imhotep.php@gmail.com>
    Date:   Sun Nov 10 17:52:31 2024 +0600
    
        Fix copyDirectory options type
    
    commit 3aa0b010d32d1523820eca70ac2cfc562bfac901
    Author: Imhotep <imhotep.php@gmail.com>
    Date:   Sun Nov 10 17:50:37 2024 +0600
    
        Update

--- src/Console/Output/ConsoleOutput.php
+++ src/Console/Output/ConsoleOutput.php
@@ -14,14 +14,88 @@ class ConsoleOutput extends StreamOutput
 
     protected int $lines = 0;
 
+    protected static ?bool $tput = null;
+
+    protected static ?bool $stty = null;
+
     public function __construct()
     {
         parent::__construct($this->getOutputStream());
 
         $this->stderr = new StreamOutput($this->getErrorStream());
 
+        $this->initDimensions();
+    }
+
+    protected function initDimensions(): void
+    {
+        // Windows
+        if (DIRECTORY_SEPARATOR === '\\') {
+            if (preg_match('/^(\d+)x(\d+)(?: \((\d+)x(\d+)\))?$/', trim(getenv('ANSICON')), $match)) {
+                $this->cols = (int)$match[1];
+                $this->lines = isset($match[4]) ? (int)$match[4] : (int)$match[2];
+                return;
+            }
+        }
+
+        if ($this->hasTputAvailable()) {
+            $this->useTputDimensions();
+        }
+        elseif ($this->hasSttyAvailable()){
+            $this->useSttyDimensions();
+        }
+    }
+
+    protected function hasTputAvailable(): bool
+    {
+        if (! is_null(static::$tput)) {
+            return static::$tput;
+        }
+
+        if (! function_exists('exec')) {
+            return false;
+        }
+
+        exec("tput 2>&1", $output, $code);
+
+        return static::$tput = ($code === 0);
+    }
+
+    protected function useTputDimensions(): void
+    {
         $this->cols = (int)exec('tput cols');
         $this->lines = (int)exec('tput lines');
+    }
+
+    protected function hasSttyAvailable(): bool
+    {
+        if (! is_null(static::$stty)) {
+            return static::$stty;
+        }
+
+        if (! function_exists('exec')) {
+            return false;
+        }
+
+        exec("stty 2>&1", $output, $code);
+
+        return static::$stty = ($code === 0);
+    }
+
+    protected function useSttyDimensions(): void
+    {
+        $stty = exec('stty -a | grep columns');
+
+        if (preg_match('/rows.(\d+);.columns.(\d+);/i', $stty, $match)) {
+            // extract from "rows N; columns N;"
+            $this->cols = (int) $match[2];
+            $this->lines = (int) $match[1];
+        }
+        elseif (preg_match('/;.(\d+).rows;.(\d+).columns/i', $stty, $match)) {
+            // extract from "; N rows; N columns"
+            $this->cols = (int) $match[2];
+            $this->lines = (int) $match[1];
+        }
     }
 
     protected function getOutputStream(): mixed
