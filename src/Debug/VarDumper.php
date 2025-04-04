95b94aa7dc0ad625ab6a10fbebb8c84d5d849351
Imhotep <imhotep.php@gmail.com>
2025-04-04 13:47:20 +0600

    Fixs..

--- src/Debug/VarDumper.php
+++ src/Debug/VarDumper.php
@@ -2,42 +2,49 @@
 
 namespace Imhotep\Debug;
 
+use Imhotep\Debug\Dumper\CliDumper;
 use Imhotep\Debug\Dumper\HtmlDumper;
 use Imhotep\Debug\Dumper\VarCloner;
 
 class VarDumper
 {
-    private static $handler = null;
+    private static mixed $handler = null;
 
     public static function dump(mixed $var): mixed
     {
-        if (is_null(self::$handler)) {
-            self::register();
+        if (is_null(static::$handler)) {
+            static::register();
         }
 
-        return (self::$handler)($var);
+        return (static::$handler)($var);
     }
 
     public function setHandler(callable $callable = null): ?callable
     {
-        $prevHandler = self::$handler;
+        $prevHandler = static::$handler;
 
         // Prevent replacing the handler with expected format as soon as the env var was set:
         if (isset($_SERVER['VAR_DUMPER_FORMAT'])) {
             return $prevHandler;
         }
 
-        self::$handler = $callable;
+        static::$handler = $callable;
 
         return $prevHandler;
     }
 
     public static function register(): void
     {
-        $dumper = new HtmlDumper();
+        if (in_array(PHP_SAPI, ['cli', 'phpdbg'])) {
+            $dumper = new CliDumper();
+        }
+        else {
+            $dumper = new HtmlDumper();
+        }
+
         $cloner = new VarCloner();
 
-        self::$handler = function ($var) use ($dumper, $cloner) {
+        static::$handler = function ($var) use ($dumper, $cloner) {
             $dumper->dump($cloner->cloneVar($var));
         };
     }
