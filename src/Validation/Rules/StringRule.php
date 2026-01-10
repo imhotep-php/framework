d9ae0b4d421305bf507e84845e451135b0421f22
Imhotep <120575926+imhotep-php@users.noreply.github.com>
2026-01-10 16:38:08 +0600

    fix(validation): numeric value can also be a string

--- src/Validation/Rules/StringRule.php
+++ src/Validation/Rules/StringRule.php
@@ -2,10 +2,21 @@
 
 namespace Imhotep\Validation\Rules;
 
-class StringRule extends AbstractRule
+use Imhotep\Contracts\Validation\IModifyValue;
+
+class StringRule extends AbstractRule implements IModifyValue
 {
     public function check(mixed $value): bool
     {
         return is_string($value);
+    }
+
+    public function modifyValue(mixed $value): mixed
+    {
+        if (is_numeric($value)) {
+            return (string)$value;
+        }
+
+        return $value;
     }
 }
