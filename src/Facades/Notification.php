c4abe6d554e6fec89952d87b9582bec0ebfc8327
Imhotep <120575926+imhotep-php@users.noreply.github.com>
2026-01-10 16:28:13 +0600

    refactor(facades): View, Notification

--- src/Facades/Notification.php
+++ src/Facades/Notification.php
@@ -1,21 +1,28 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Imhotep\Facades;
 
-use Imhotep\Contracts\Notifications\Notification as NotificationContract;
+use Imhotep\Contracts\Notifications\INotification;
+use Imhotep\Contracts\Notifications\INotificationDriver;
+use Imhotep\Notifications\AnonymousRecipient;
+use Imhotep\Notifications\ChannelManager;
 
 /**
- * @method static void send($recipients, NotificationContract $notification)
- * @method static void sendNow($recipients, NotificationContract $notification)
+ * @method static void send(mixed $recipients, INotification $notification)
+ * @method static void sendNow(mixed $recipients, INotification $notification)
+ * @method static ChannelManager locale(string $locale)
+ * @method static INotificationDriver channel(?string $name = null)
+ * @method static AnonymousRecipient route(string $driver)
+ * @method static AnonymousRecipient routes(array $routes)
+ * @method static string getDefaultChannel()
+ * @method static string setDefaultChannel(string $channel)
  *
- * @see \Imhotep\Notifications\ChannelManager
+ * @see ChannelManager
  */
 class Notification extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
-        return 'notify';
+        return 'notification';
     }
 }
